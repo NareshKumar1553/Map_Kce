323a230c3b9b7fa30e4b514d11c01f3f0d66c0e7
NareshKumar1553 <nareshkumar.s1553@gmail.com>
2023-03-24 21:42:18 +0530

    commit

--- lib/Blocks/BlockLabList.jsx
+++ lib/Blocks/BlockLabList.jsx
@@ -5,15 +5,16 @@ import { View, Text,Image,ScrollView } from 'react-native';
 import Anim from '../Pages/Animation';
 
 function BlockLabList({ navigation,route }) {
-    let bgColor = '#c8b0f5'
-    useEffect(()=>{
-        StatusBar.setBarStyle( 'light-content',true)
-        StatusBar.setBackgroundColor(bgColor)
-      }) 
+    let bgColor = 'white';
+   useEffect(()=>{
+    StatusBar.setBarStyle( 'light-content',true)
+    StatusBar.setBackgroundColor('#78756e')
+    })
     var img = [];
     const {name,Labname} = route.params;
     const [loading, setLoading] = useState(true); // Set loading to true on component mount
     const [users, setUsers] = useState([]); // Initial empty array of users
+    console.log("NeededImage ",NeededImage);
     useEffect(() => {
         const subscriber = firestore()
             .collection(name)
@@ -40,18 +41,19 @@ function BlockLabList({ navigation,route }) {
     users.map(user => {
         console.log({user}),
         img.push(
-            <View style={{backgroundColor:"#c8b0f5"}}>
+            <View style={{backgroundColor:"white"}}>
                 <TouchableOpacity 
                 onPress={()=>{navigation.push('BDetails',{user})}}
-                style={{width:345,height:65,backgroundColor:'white',borderRadius:20,marginBottom:10}}>
+                style={{width:345,height:65,backgroundColor:'#febdff',borderRadius:20,marginBottom:10}}>
                 <Text style={{textAlign:'center',color:'black',fontWeight:'bold',fontSize:24,paddingTop:12}}>{user.name}</Text>
                 </TouchableOpacity>
             </View>
         )
     });
     return(
-        console.log("A Block Page"),
-        <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'#c8b0f5'}}>
+        console.log("Block List Page"),
+        <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
+            <Image source={{uri:"https://github.com/pavanpk811/KCE_Map/raw/main/"+name+".png"}} style={{width:'100%',height:250}}/>
         <ScrollView >
             <Text style={{fontWeight:'bold',paddingBottom:15,textAlign:'center',color:'black',marginTop:15,fontSize:32}}>{Labname}</Text>
             {img}
